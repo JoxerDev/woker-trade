cdd3d4468b230807ea2a0ef083f1515d5c62118b
JoxerDev <papp.peter.tamas@gmail.com>
2022-02-10 01:26:52 +0100

    reb4

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   nav2Items = [
-    {name: 'fejlesztes', img: 'uszt120.png', link: ''},
-    {name: 'fejlesztes2', img: 'infoblokk_kedv_final_CMYK_ERFA.jpg', link: ''},
-    {name: 'fejlesztes3', img: 'infoblokk_esza.png', link: ''},
+    {name: 'fejlesztes', img: 'uszt120.png', link: 'fejlesztes'},
+    {name: 'fejlesztes2', img: 'infoblokk_kedv_final_CMYK_ERFA.jpg', link: 'fejlesztes2'},
+    {name: 'fejlesztes3', img: 'infoblokk_esza.png', link: 'fejlesztes3'},
   ];
 
   constructor() { }
